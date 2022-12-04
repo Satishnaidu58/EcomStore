c146a3a89959462ed560c9b6ec46faf7b30995f4
SATISH KUMAR NAIDU <88018904+Satishnaidu58@users.noreply.github.com>
2022-12-04 11:18:06 +0530

    Update settings.py

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR/ 'static/images'
