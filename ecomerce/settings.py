ef16b64522b7ff3a0996727d082ef25697caa52c
SATISH KUMAR NAIDU <88018904+Satishnaidu58@users.noreply.github.com>
2023-01-11 12:58:21 +0530

    Update settings.py

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles_build', 'static')
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles_build')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
